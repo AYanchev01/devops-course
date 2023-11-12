876770134d4df9e4c5698f450cb41aaec98df374
Anton Yanchev <ayanchev01@gmail.com>
2023-11-13 00:50:28 +0200

    fix lint issue in test

--- src/app_test.py
+++ src/app_test.py
@@ -7,7 +7,6 @@ class TestApp(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
 
-
     def test_hello_world(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
